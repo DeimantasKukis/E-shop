1ab8fa444a98b07d3ad6b591d0cfe159678ebe0e
Deimantas <Deimantas.Kukis@gmail.com>
2021-10-15 15:50:36 +0300

    updated

--- frontend/js/library.js
+++ frontend/js/library.js
@@ -4,7 +4,7 @@ function displayProducts(products, element) {
     const product = products[index];
 
     let template =
-      '<div class="col m4 s12"><div class="card"><div class="card-image waves-effect waves-block waves-light"> <img class="activator" src="{{photo}}"></div><div class="card-content"> <span class="card-title activator grey-text text-darken-4"> {{productName}} <br> Kaina: {{finalPrice}} € </br><i class="material-icons right" >more_vert</i > </span ><p> </p></div><div class="card-reveal"> <span class="card-title grey-text text-darken-4" >Pradinė Kaina: {{startPrice}} <i class="material-icons right">close</i> </span ><p> Produkto numeris: {{productId}} <br>Produkto aprašymas: {{productDescription}}</p></div></div></div>';
+      '<div class="col m4 s12"><div class="card"><div class="card-image waves-effect waves-block waves-light"> <img class="activator" src="{{photo}}"></div><div class="card-content"> <span class="card-title activator grey-text text-darken-4"> {{productName}} <br> Kaina: {{finalPrice}} € </br><i class="material-icons right" >more_vert</i > </span ><p> </p></div><div class="card-reveal"> <span class="card-title grey-text text-darken-4" >Pradinė Kaina: {{startPrice}} €<i class="material-icons right">close</i> </span ><p>  <br>Produkto aprašymas: {{productDescription}}</p></div></div></div>';
 
     template = template.replaceAll("{{photo}}", product.photo);
     template = template.replaceAll("{{productId}}", product.productId);
